d61fe8a1154fccb0b08a67138c5f83ffd02116fa
Michel de Wijk <freky_dewiek@hotmail.com>
2016-01-14 14:35:47 +0100

    Update nav bar

--- index.php
+++ index.php
@@ -2,115 +2,14 @@
 error_reporting( E_ALL ); 
 ini_set( "display_errors", 1 );   
 
-$string = file_get_contents("http://datasets.antwerpen.be/v4/gis/bbq.json");
-$jsonRS = json_decode ($string,true);
-$locatie = "";
+include("/nav.php");
+
 ?>
-	<table border="5">
-	<tr>
-		<td width="40px"><strong><?php echo "ID"?></td>
-		<td width="70px"><strong><?php echo "Objectid"?></td>
-		<td width="150px"><strong><?php echo "Point_lat"?></td>
-		<td width="150px"><strong><?php echo "point_lng"?></td>
-		<td width="70px"><strong><?php echo "Obj_type"?></td>
-		<td width="40px"><strong><?php echo "O_id"?></td>
-		<td width="150px"><strong><?php echo "Ligging"?></td>
-		<td width="70px"><strong><?php echo "Status"?></td>
-	</tr>
-	</table>
-<?php
 
-foreach ($jsonRS["data"] as $rs) 
-{
-?>	
-	<table border="1">
-	<tr>
-		<td width="40px"><?php echo $rs["id"]?></td>
-		<td width="70px"><?php echo $rs["objectid"]?></td>
-		<td width="150px"><?php echo $rs["point_lat"]?></td>
-		<td width="150px"><?php echo $rs["point_lng"]?></td>
-		<td width="70px"><?php echo $rs["obj_type"]?></td>
-		<td width="40px"><?php echo $rs["o_id"]?></td>
-		<td width="150px"><?php echo $rs["ligging"]?></td>
-		<td width="70px"><?php echo $rs["status"]?></td>
-		
-	</tr>
-	</table>
-	
-<?php
-}
-?> 
-
-<!DOCTYPE HTML>
-<html>
-<head>
-<style>
-.error {color: #FF0000;}
-</style>
-</head>
 <body>
+<html>
 
-<?php	
-	$locatie = "";
-	if ($_SERVER["REQUEST_METHOD"] == "POST") {
-		
-	
-   if (empty($_POST["locatie"])) {
-   } else {
-     $locatie = test_input($_POST["locatie"]);
-   }
-}
 
-function test_input($data) {
-   $data = trim($data);
-   $data = stripslashes($data);
-   $data = htmlspecialchars($data);
-   return $data;
-}
-?>
-
-<form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>"> <br>
-   Locatie:
-   <input type="radio" name="locatie" <?php if (isset($locatie) && $locatie=="PARK") echo "checked";?>  value="PARK">Park
-   <input type="radio" name="locatie" <?php if (isset($locatie) && $locatie=="GROENZONE") echo "checked";?>  value="GROENZONE">Groenzone
-   <input type="radio" name="locatie" <?php if (isset($locatie) && $locatie=="PLEIN") echo "checked";?>  value="PLEIN">Plein
-   <br><br>
-   <input type="submit" name="submit" value="Filter">
-</form>
-
-<?php
-if($locatie != null)
-{
-	echo "<h2>Gekozen filtering: $locatie</h2>";
-
-	foreach ($jsonRS["data"] as $rs) 
-	{
-		if($rs["ligging"] == $locatie)
-		{
-?>	
-		<table border="1">
-		<tr>
-			<td width="40px"><?php echo $rs["id"]?></td>
-			<td width="70px"><?php echo $rs["objectid"]?></td>
-			<td width="150px"><?php echo $rs["point_lat"]?></td>
-			<td width="150px"><?php echo $rs["point_lng"]?></td>
-			<td width="70px"><?php echo $rs["obj_type"]?></td>
-			<td width="40px"><?php echo $rs["o_id"]?></td>
-			<td width="150px"><?php echo $rs["ligging"]?></td>
-			<td width="70px"><?php echo $rs["status"]?></td>
-			
-		</tr>
-		</table>
-		
-<?php
-
-		}
-		else
-		{
-		}	
-	} 
-}
-?>
 
 </body>
 </html>
---
+++ nav.php
@@ -0,0 +1,22 @@
+<?php
+error_reporting( E_ALL ); 
+ini_set( "display_errors", 1 );   
+
+?>
+
+<body>
+<html>
+
+	<form action="/project/project-web/main.php">
+		<input type="submit" value="Main Pagina">
+	</form>
+	
+	<form action="http://google.com">
+		<input type="submit" value="Google Maar!">
+	</form>
+
+</body>
+</html>
+
+
+<link rel="stylesheet" href="bootstrap/css/bootstrap.css" type="text/css">
