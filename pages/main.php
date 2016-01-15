57b3a9fa8759526e2732ac55b14d108977b2cf5e
nivle <lumani.elvin@gmail.com>
2016-01-15 02:07:57 +0100

    ()

---
+++ pages/main.php
@@ -0,0 +1,163 @@
+
+<?php
+error_reporting( E_ALL ); 
+ini_set( "display_errors", 1 );   
+
+include("nav.php");
+
+$string = file_get_contents("../bbq.json");
+$jsonRS = json_decode ($string,true);
+$locatie = "";
+$link = "";
+?>
+<!DOCTYPE HTML>
+<html>
+<head>
+<style>
+.error {color: #FF0000;}
+</style>
+</head>
+<body>
+
+<div class="panel panel-default">
+<div class="panel-body">
+<table class="table table-bordered table-responsive">
+<tr>
+<td><strong><?php echo "ID"?></td>
+<td><strong><?php echo "Objectid"?></td>
+<td><strong><?php echo "Point_lat"?></td>
+<td><strong><?php echo "point_lng"?></td>
+<td><strong><?php echo "Obj_type"?></td>
+<td><strong><?php echo "O_id"?></td>
+<td><strong><?php echo "Ligging"?></td>
+<td><strong><?php echo "Status"?></td>
+<td><strong><?php echo "Link"?></td>
+</tr>
+
+<?php
+foreach ($jsonRS["data"] as $rs) {
+?>
+<tr>
+<td><?php echo $rs["id"]?></td>
+<td><?php echo $rs["objectid"]?></td>
+<td><?php echo $rs["point_lat"]?></td>
+<td><?php echo $rs["point_lng"]?></td>
+<td><?php echo $rs["obj_type"]?></td>
+<td><?php echo $rs["o_id"]?></td>
+<td><?php echo $rs["ligging"]?></td>
+<td><?php echo $rs["status"]?></td>
+<?php
+$link = "https://www.google.be/maps/place//@".$rs["point_lat"].",".$rs["point_lng"].",17z/";	
+?>
+<td><a class = "btn btn-default" href="<?php echo $link; ?>">Google Maps</a></td>
+</tr>
+
+<?php
+}
+?>
+
+</table>
+</div>
+</div>  
+
+<?php	
+$locatie = "";
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+
+
+if (empty($_POST["locatie"])) {
+} else {
+$locatie = test_input($_POST["locatie"]);
+}
+}
+
+function test_input($data) {
+$data = trim($data);
+$data = stripslashes($data);
+$data = htmlspecialchars($data);
+return $data;
+}
+?>
+
+<form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
+
+
+
+
+<div class="panel panel-default">
+<div class="panel-heading">
+Locatie:
+<input type="radio" name="locatie" <?php if (isset($locatie) && $locatie=="PARK") echo "checked";?>  value="PARK">Park
+<input type="radio" name="locatie" <?php if (isset($locatie) && $locatie=="GROENZONE") echo "checked";?>  value="GROENZONE">Groenzone
+<input type="radio" name="locatie" <?php if (isset($locatie) && $locatie=="PLEIN") echo "checked";?>  value="PLEIN">Plein
+</div>
+    
+  
+    
+<div class="panel-body">
+<input class = "btn btn-default" type="submit" name="submit" value="Filter">
+</div>
+</div>
+
+
+<div class="panel panel-default">
+<div class="panel-heading"><?php
+echo "<h2>Gekozen filtering: $locatie</h2>";
+?></div>
+<div class="panel-body">
+
+
+<table class="table table-bordered table-responsive">
+<tr>
+<td><strong><?php echo "ID"?></td>
+<td><strong><?php echo "Objectid"?></td>
+<td><strong><?php echo "Point_lat"?></td>
+<td><strong><?php echo "point_lng"?></td>
+<td><strong><?php echo "Obj_type"?></td>
+<td><strong><?php echo "O_id"?></td>
+<td><strong><?php echo "Ligging"?></td>
+<td><strong><?php echo "Status"?></td>
+<td><strong><?php echo "Link"?></td>
+</tr>
+
+
+
+<?php
+if($locatie != null)
+{
+
+
+foreach ($jsonRS["data"] as $rs) 
+{
+if($rs["ligging"] == $locatie)
+{
+?>	
+
+<tr>
+<td><?php echo $rs["id"]?></td>
+<td><?php echo $rs["objectid"]?></td>
+<td><?php echo $rs["point_lat"]?></td>
+<td><?php echo $rs["point_lng"]?></td>
+<td><?php echo $rs["obj_type"]?></td>
+<td><?php echo $rs["o_id"]?></td>
+<td><?php echo $rs["ligging"]?></td>
+<td><?php echo $rs["status"]?></td>
+<?php
+$link = "https://www.google.be/maps/place//@".$rs["point_lat"].",".$rs["point_lng"].",17z/";	
+?>
+<td><a class = "btn btn-default" href="<?php echo $link; ?>">Google Maps</a></td>
+</tr>
+<?php
+}
+else
+{
+}	
+} 
+}
+?>
+</table>
+</div>
+</div>
+
+</body>
+</html>
---
+++ pages/nav.php
@@ -0,0 +1,39 @@
+<?php
+error_reporting( E_ALL ); 
+ini_set( "display_errors", 1 );   
+
+?>
+<html>
+<header>
+<title>Barbecue</title>
+<link rel="stylesheet" href="../bootstrap/css/bootstrap.css" type="text/css">
+</header>
+<body>
+
+<nav class="navbar navbar-default">
+
+<ul class="nav navbar-nav nav-tabs">
+<li><a href="index.php">Home</a></li>
+<li><a href="main.php">Locaties</a></li>
+<!--        <li><a href="https://www.google.com">Google</a></li>-->
+</ul>
+
+<form class="navbar-form navbar-left" role="search">
+<div class="form-group">
+<input type="text" class="form-control" placeholder="Search">
+</div>
+<button type="submit" class="btn btn-default">Google Search</button>
+</form>
+
+</nav>
+
+<script src="../bootstrap/js/bootstrap.js"></script>
+<script src="//code.jquery.com/jquery-1.12.0.min.js"></script>
+
+
+</body>
+</html>
+
+
+
+
