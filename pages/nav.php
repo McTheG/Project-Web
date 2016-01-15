9853723f12d07526d64110e7506ad8a3af91c67c
nivle <lumani.elvin@gmail.com>
2016-01-15 14:37:44 +0100

    ()

--- pages/nav.php
+++ pages/nav.php
@@ -9,6 +9,8 @@ ini_set( "display_errors", 1 );
 <title>Barbecue Map</title>
 <link rel="stylesheet" href="../reset.css" type="text/css">
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
+<link rel="stylesheet" href="../mozzilla.css" type="text/css">
+ 
     
 </header>
 <body>
@@ -34,7 +36,6 @@ ini_set( "display_errors", 1 );
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>
 <script src="http://code.angularjs.org/1.2.6/angular.min.js"></script>
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
-
 
 
 </body>
